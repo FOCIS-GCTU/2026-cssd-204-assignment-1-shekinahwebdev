4e953e6eaee38c870ae0b3c5bff1ca2dfdc2a74e
Patricia Shiloh Kanneh <148111142+shekinahwebdev@users.noreply.github.com>
2026-05-30 23:20:09 +0000

    Implement initials printing in stylized format
    This script prints the initials 'PSK' in stylized large letters.

--- initials.py
+++ initials.py
@@ -1,7 +1,28 @@
+# File: initials.py
+# Description: This script prints the initials 'PSK' in stylized large letters.
+# Assignment: 1
+# Name: Patricia Shiloh Kanneh
+# STUDENT ID: 2425402386
+# Email: 2424502386@live.gctu.edu.gh
+# Grader: Mr. Augustime Buckman
+# On my honor, Patricia Shiloh Kanneh, this programming assignment is my own work and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+  # This functions prints the initials 'PSK' in stylized large letters
+    print()
+    print("...PSK")
+    print()
 
-
-
+    print("...PPPPPPPPPPPP..........SSSSSSSSSS........KK........KK.....")
+    print("...PP........PP.........SSSSSSSSSSS........KK.......KK......")
+    print("...PP........PP........SSSS................KK......KK.......")
+    print("...PP........PP........SSSS................KK....KK.........")
+    print("...PPPPPPPPPPPP..........SSSSSS............KK...KK..........")
+    print("...PPPPPPPPPPPP............SSSSSS..........KKKK.KK..........")
+    print("...PP.........................SSSS.........KK....KK.........")
+    print("...PP.........................SSSS.........KK.....KK........")
+    print("...PP.............**...SSSSSSSSSS.....**...KK......KK.....**")
+    print("...PP.............**...SSSSSSSSS......**...KK........KK...**")
+    print()
+  
 main()
